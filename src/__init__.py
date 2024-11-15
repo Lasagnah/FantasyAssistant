2ff60df88d16536987c52e1ca5c538af787cf3fa
akhiljobi003 <160806209+akhiljobi003@users.noreply.github.com>
2024-11-15 11:20:10 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
+import sys
 from database import setup_database
 from draft import draft_players
+from prediction_method import display_prediction_results
 
 if __name__ == "__main__":
     setup_database()
